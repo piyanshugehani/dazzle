399cd5dedaded1d67b52f665e7f102078e266cce
piyanshugehani <139869125+piyanshugehani@users.noreply.github.com>
2025-02-02 18:44:29 +0530

    ..

--- components/MarqueeComp.tsx
+++ components/MarqueeComp.tsx
@@ -92,8 +92,8 @@ export function MarqueeDemo() {
           <ReviewCard key={review.username} {...review} />
         ))}
       </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-[#f5ede4] dark:from-[#E6D0B3]"></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/5 bg-gradient-to-l from-[#fdf3e9] dark:from-[#E6D0B3]"></div>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-[#f5ede4] dark:from-[#E6D0B3]"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-[#fdf3e9] dark:from-[#E6D0B3]"></div>
     </div>
   );
 }
